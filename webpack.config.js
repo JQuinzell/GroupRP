61495409e5296fbcc54823dc01e06921f4f5c97e
MrRainwater <jqrvz6@mst.edu>
2018-01-07 08:08:43 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
