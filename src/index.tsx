4ca5d827b9735bf68bb17a24186b60bfee5b0568
MrRainwater <jqrvz6@mst.edu>
2018-06-08 15:39:52 -0500

    test graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,26 @@ import * as ReactDOM from 'react-dom'
 import Playlist from './components/Playlist'
 import {initializeStores} from 'stores'
 import RoomStore from 'stores/RoomStore'
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag'
+
+const client = new ApolloClient({
+    uri: 'http://localhost:3000/graphql'
+})
+
+client
+    .query({
+        query: gql`
+            {
+                allAuthors {
+                    id
+                }
+            }
+        `
+    })
+    .then(console.log)
+
+    .catch(err => console.log('wtf'))
 
 initializeStores()
 
