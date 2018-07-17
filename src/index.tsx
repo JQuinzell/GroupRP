daaa9ab0693e32e4e86817b41c054f13655e7bd9
MrRainwater <jqrvz6@mst.edu>
2018-07-16 20:13:48 -0500

    use css baseline

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import {Provider} from 'mobx-react'
 import {initializeStores} from 'stores'
 import GroupListing from 'components/groups/GroupListing'
 import GroupStore from 'stores/GroupStore'
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 initializeStores()
 
 ReactDOM.render(
     <Provider
         GroupStore={GroupStore}>
-        <GroupListing />
+        <React.Fragment>
+            <CssBaseline />
+            <GroupListing />
+        </React.Fragment>
     </Provider>,
     document.getElementById('app')
 )
