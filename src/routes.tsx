32fcd6d48ab0b3fe6d0bc20ad81edc4460aac6b3
MrRainwater <jqrvz6@mst.edu>
2018-07-21 15:22:44 -0500

    route for room listing

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import GroupListing from 'components/groups/GroupListing'
 import ChatRoom from 'components/chat/ChatRoom'
+import ChatListing from 'components/chat/ChatListing'
 
 export default () => {
     return (
         <Router>
             <div>
                 <Route exact path="/" component={GroupListing} />
+                <Route path="/groups/:id/rooms" component={ChatListing} />
                 <Route path="/chat" component={ChatRoom} />
             </div>
         </Router>
