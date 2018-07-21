3d95606bd8106bf7a366e3f400782d8ed418e370
MrRainwater <jqrvz6@mst.edu>
2018-07-21 11:37:01 -0500

    add chat route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import GroupListing from 'components/groups/GroupListing'
+import ChatRoom from 'components/ChatRoom'
 
 export default () => {
     return (
         <Router>
-            <Route exact path="/" component={GroupListing} />
+            <div>
+                <Route exact path="/" component={GroupListing} />
+                <Route path="/chat" component={ChatRoom} />
+            </div>
         </Router>
     )
 }
