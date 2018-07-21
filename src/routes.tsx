860f9f3f9c225fe8e4d34fb5f7a9be66902daf8a
MrRainwater <jqrvz6@mst.edu>
2018-07-21 15:13:45 -0500

    use new chat file

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import GroupListing from 'components/groups/GroupListing'
-import ChatRoom from 'components/ChatRoom'
+import ChatRoom from 'components/chat/ChatRoom'
 
 export default () => {
     return (
