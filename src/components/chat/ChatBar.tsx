3ba38d7e2eb0171aae0ca8e4ebfd93d25c03a68f
Jared Rainwater <jqrvz6@mst.edu>
2018-11-02 18:12:20 -0500

    rework chatbar

--- src/components/chat/ChatBar.tsx
+++ src/components/chat/ChatBar.tsx
@@ -2,86 +2,95 @@ import * as React from 'react'
 import Post from 'data/post'
 import './styles/ChatBar.scss'
 
-interface Tabs {
-    [name: string]: Post[]
+interface Room {
+  name: string
+  posts: Post[]
 }
 
 interface Props {
-    tabs: Tabs
-    selectedTab: string
-    selectTab: (tab: string) => void
-    sendMessage: (message: string) => void
+  rooms: Room[]
+  selectedRoom: string
+  selectTab: (tab: string) => void
+  sendMessage: (message: string) => void
 }
 
 interface State {
-    message: string
+  message: string
 }
 
 export default class ChatBar extends React.Component<Props, State> {
-    state = {
-        message: '',
+  state = {
+    message: ''
+  }
+
+  chatboxRef = React.createRef<HTMLDivElement>()
+
+  handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      this.handleSubmit()
     }
+  }
 
-    chatboxRef = React.createRef<HTMLDivElement>()
+  handleChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    this.setState({
+      message: e.currentTarget.value
+    })
+  }
 
-    handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        if (e.key === 'Enter') {
-            e.preventDefault()
-            this.handleSubmit()
-        }
-    }
+  handleSubmit = () => {
+    const message = this.state.message
+    this.setState({message: ''})
+    this.props.sendMessage(message)
+  }
 
-    handleChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
-        this.setState({
-            message: e.currentTarget.value
-        })
-    }
+  moveScrollBarToBottom = () => {
+    this.chatboxRef.current.scrollTop = this.chatboxRef.current.scrollHeight
+  }
 
-    handleSubmit = () => {
-        const message = this.state.message
-        this.setState({ message: '' })
-        this.props.sendMessage(message)
-    }
+  componentDidMount() {
+    this.moveScrollBarToBottom()
+  }
 
-    moveScrollBarToBottom = () => {
-        this.chatboxRef.current.scrollTop = this.chatboxRef.current.scrollHeight
-    }
+  componentDidUpdate() {
+    this.moveScrollBarToBottom()
+  }
 
-    componentDidMount() {
-        this.moveScrollBarToBottom()
-    }
+  render() {
+    const selectedRoom = this.props.rooms.find(
+      room => room.name === this.props.selectedRoom
+    )
 
-    componentDidUpdate() {
-        this.moveScrollBarToBottom()
-    }
-
-    render() {
-        const tabs = Object.keys(this.props.tabs)
-        const selectedMessages = this.props.tabs[this.props.selectedTab] || []
-
-        return (
-            <div className="ChatBar">
-                <div className="tabs">
-                {
-                    tabs.map(name => (
-                        <div className={name === this.props.selectedTab ? 'selected tab' : 'tab'} 
-                             onClick={() => this.props.selectTab(name)} 
-                             key={name}>
-                            {name}
-                        </div>
-                    ))
-                }
-                </div>
-                <div ref={this.chatboxRef} className="messages">
-                    {selectedMessages.map((msg, i) => (
-                        <div key={i} className="message">
-                            <span className="username">{msg.username}:</span>
-                            {msg.body}
-                        </div>
-                    ))}
-                </div>
-                <textarea value={this.state.message} placeholder="Type here ..." onKeyDown={this.handleKeyDown} onChange={this.handleChange}/>
+    return (
+      <div className="ChatBar">
+        <div className="tabs">
+          {this.props.rooms.map(room => (
+            <div
+              className={
+                room.name === this.props.selectedRoom ? 'selected tab' : 'tab'
+              }
+              onClick={() => this.props.selectTab(room.name)}
+              key={room.name}
+            >
+              {room.name}
             </div>
-        )
-    }
+          ))}
+        </div>
+        <div ref={this.chatboxRef} className="messages">
+          {selectedRoom.posts.map(post => (
+            <div key={post._id} className="message">
+              <span className="username">{post.username}:</span>
+              {post.body}
+            </div>
+          ))}
+        </div>
+        <textarea
+          value={this.state.message}
+          placeholder="Type here ..."
+          onKeyDown={this.handleKeyDown}
+          onChange={this.handleChange}
+        />
+      </div>
+    )
+  }
 }
