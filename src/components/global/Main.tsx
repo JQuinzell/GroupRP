14e792e64ed3c4824e9b957d00252641c2cf707f
Jared Rainwater <jqrvz6@mst.edu>
2018-11-09 23:04:06 -0600

    joined groups list

--- src/components/global/Main.tsx
+++ src/components/global/Main.tsx
@@ -2,14 +2,14 @@ import * as React from 'react'
 import Drawer from '@material-ui/core/Drawer'
 import Person from '@material-ui/icons/Person'
 import Grid from '@material-ui/core/Grid'
-import { withStyles, createStyles, WithStyles } from '@material-ui/core';
+import { withStyles, createStyles, WithStyles, List, ListSubheader, ListItem } from '@material-ui/core'
 
-const drawerWidth = 400
+const drawerWidth = 300
 
 const styles = createStyles({
     drawer: {
         width: drawerWidth,
-        flexShrink: 0,
+        flexShrink: 0
     },
     drawerPaper: {
         width: drawerWidth
@@ -20,28 +20,38 @@ const styles = createStyles({
     userIcon: {
         width: 75,
         height: 75,
-        color: 'white',
+        color: 'white'
     },
     userIconCell: {
         borderRadius: '100%',
         background: '#379096',
         padding: 5
+    },
+    lists: {
+        alignSelf: 'stretch'
     }
 })
 
 interface Props extends WithStyles<typeof styles> {}
 interface State {}
 
-class Main extends React.Component<Props, State>{
+class Main extends React.Component<Props, State> {
     render() {
         const { classes } = this.props
 
         return (
             <React.Fragment>
-                <Drawer className={classes.drawer} classes={{paper: classes.drawerPaper}} variant='permanent'>
+                <Drawer className={classes.drawer} classes={{ paper: classes.drawerPaper }} variant="permanent">
                     <Grid container alignItems="center" direction="column" className={classes.container}>
                         <Grid className={classes.userIconCell} item>
                             <Person className={classes.userIcon} />
+                        </Grid>
+                        <Grid item className={classes.lists}>
+                            <List subheader={<ListSubheader>Joined Groups</ListSubheader>}>
+                                {[1, 2, 3].map(i => (
+                                    <ListItem button>Group {i}</ListItem>
+                                ))}
+                            </List>
                         </Grid>
                     </Grid>
                 </Drawer>
