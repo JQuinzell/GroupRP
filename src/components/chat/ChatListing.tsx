8da623336bf27a19a861b91e0a383f0ad47c47b0
Jared Rainwater <jqrvz6@mst.edu>
2018-11-17 14:16:33 -0600

    remove old code

--- src/components/chat/ChatListing.tsx
+++ src/components/chat/ChatListing.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
-import { inject, observer } from "mobx-react";
+import { inject, observer } from 'mobx-react'
 import { Link } from 'react-router-dom'
-import Group from 'models/Group'
-import Room from 'models/Room'
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 
 interface Props {
     group: {
@@ -20,7 +18,7 @@ interface Props {
 
 export default class RoomListing extends React.Component<Props, {}> {
     render() {
-        const {rooms, group} = this.props
+        const { rooms, group } = this.props
         return (
             <div className="card-listing">
                 <Typography align="center" variant="display2">
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,18 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {Provider} from 'mobx-react'
-import {initializeStores} from 'stores'
-import GroupStore from 'stores/GroupStore'
-import RoomStore from 'stores/RoomStore'
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from '@material-ui/core/CssBaseline'
 import Routes from './routes'
-import {client} from 'api/client'
+import { client } from 'api/client'
 import { ApolloProvider } from 'react-apollo'
 
 import 'components/global/styles/CardListing.scss'
 
-initializeStores()
-
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <Provider
-            GroupStore={GroupStore}
-            RoomStore={RoomStore}>
-            <React.Fragment>
-                <CssBaseline />
-                <Routes />
-            </React.Fragment>
-        </Provider>
+        <React.Fragment>
+            <CssBaseline />
+            <Routes />
+        </React.Fragment>
     </ApolloProvider>,
     document.getElementById('app')
 )
