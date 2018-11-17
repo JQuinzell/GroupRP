dab1e4b5b9379a8d5befce599f4efebfbc9bed3f
Jared Rainwater <jqrvz6@mst.edu>
2018-11-17 14:15:15 -0600

    setup client state and remove old query calls

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,64 +1,11 @@
 import ApolloClient from 'apollo-boost'
-import gql from 'graphql-tag'
-import Group from 'models/Group'
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql'
-})
-
-export default {
-    groups: {
-        all(): Promise<Group[]> {
-            return client.query({
-                query: gql`
-                {
-                    groups {
-                        _id
-                        name
-                        description
-                        rooms {
-                            _id
-                            name
-                        }
-                    }
-                }
-                `
-            })
-            .then(resp => resp.data['groups'])
+    uri: 'http://localhost:4000/graphql',
+    clientState: {
+        defaults: {
+            selectedRoomId: null
         },
-
-        get(id: string): Promise<Group> {
-            return client.query({
-                query: gql`
-                {
-                    group(id: "${id}") {
-                        _id
-                        name
-                        description
-                        rooms {
-                            _id
-                            name
-                        }
-                    }
-                }
-                `
-            })
-            .then(resp => resp.data['group'])
-        }
-    },
-    posts: {
-        all(id: string) {
-            return client.query({
-                query: gql`
-                {
-                    posts(room: "${id}") {
-                        _id
-                        body
-                    }
-                }
-                `
-            })
-                .then(resp => resp.data['posts'])
-        }
+        resolvers: {}
     }
-}
+})
