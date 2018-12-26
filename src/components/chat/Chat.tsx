d45b9714cd0a78d6babaed1ff44f29580ba3581e
MrRainwater <n/a>
2018-12-26 17:47:24 -0600

    style chat message

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { TextField, WithStyles, createStyles, withStyles, Grid, Paper, Typography } from '@material-ui/core'
+import { Avatar, TextField, WithStyles, createStyles, withStyles, Paper, Typography } from '@material-ui/core'
 
 const styles = createStyles({
     input: {
@@ -12,13 +12,28 @@ const styles = createStyles({
         flexDirection: 'column',
         justifyContent: 'flex-end'
     },
+    avatar: {
+        float: 'left',
+        marginRight: 20,
+        shapeOutside: 'circle(50%)',
+        width: 75,
+        height: 75
+    },
     messageContainer: {
         overflow: 'auto'
     },
     messages: {},
     message: {
         margin: '15px 0',
-        padding: 15
+        padding: 15,
+        overflow: 'auto'
+    },
+    text: {
+        display: 'inline'
+    },
+    boldText: {
+        display: 'inline',
+        lineHeight: 0
     }
 })
 
@@ -43,7 +58,27 @@ class Chat extends React.Component<Props> {
                         {posts.map(post => (
                             <div className={classes.item} key={post._id}>
                                 <Paper className={classes.message}>
-                                    <Typography variant="body1">{post.body}</Typography>
+                                    <Avatar
+                                        className={classes.avatar}
+                                        src="https://i.kinja-img.com/gawker-media/image/upload/s--4LHBt0O4--/c_scale,f_auto,fl_progressive,q_80,w_800/kaprfadz9rnvypesa2u9.png"
+                                    />
+                                    <Typography variant="h6" classes={{ root: classes.boldText }}>
+                                        Username:{' '}
+                                    </Typography>
+                                    <Typography variant="body1" classes={{ root: classes.text }}>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam soluta eligendi ab dolore, enim magnam nulla
+                                        quibusdam! Voluptatem obcaecati, voluptatibus accusamus accusantium culpa officiis dolorem incidunt amet
+                                        libero quibusdam debitis voluptates, et minima deleniti ab fugiat placeat, esse aliquam eos recusandae? Velit
+                                        neque voluptate beatae! Nobis, voluptatibus delectus perferendis quis porro maxime, ab, laudantium beatae
+                                        repellendus molestiae ea culpa voluptas quasi doloremque temporibus natus enim accusamus obcaecati recusandae
+                                        possimus ipsam dolores ipsum! Perferendis, nisi facere. Ab quo obcaecati dolorum eligendi hic nulla quibusdam
+                                        ad, eveniet assumenda dolor impedit laborum a quasi molestiae quam sint, blanditiis similique dolorem nostrum
+                                        aut atque? neque voluptate beatae! Nobis, voluptatibus delectus perferendis quis porro maxime, ab, laudantium
+                                        beatae repellendus molestiae ea culpa voluptas quasi doloremque temporibus natus enim accusamus obcaecati
+                                        recusandae possimus ipsam dolores ipsum! Perferendis, nisi facere. Ab quo obcaecati dolorum eligendi hic nulla
+                                        quibusdam ad, eveniet assumenda dolor impedit laborum a quasi molestiae quam sint, blanditiis similique
+                                        dolorem nostrum aut atque?
+                                    </Typography>
                                 </Paper>
                             </div>
                         ))}
